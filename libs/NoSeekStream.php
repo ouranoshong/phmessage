a1fb0863514ebf9d6fbef88e8d88663875fc7565
ouranoshong <ouranoshong@outlook.com>
2016-10-27 23:37:30 +0800

    Add stream decorators

---
+++ libs/NoSeekStream.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hong
+ * Date: 16-10-27
+ * Time: 下午10:50
+ */
+
+namespace PhMessage;
+
+
+use Psr\Http\Message\StreamInterface;
+
+class NoSeekStream implements StreamInterface
+{
+    use handleStreamDecorator;
+
+    public function seek($offset, $whence = SEEK_SET)
+    {
+        throw new \RuntimeException('Cannot seek a NoSeekStream');
+    }
+
+    public function isSeekable()
+    {
+        return false;
+    }
+}
