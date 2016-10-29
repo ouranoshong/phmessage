bbfba6acf7597f5c7ba1b32cbede64d370449e6a
ouranoshong <ouranoshong@outlook.com>
2016-10-29 16:35:38 +0800

    Add test cases

---
+++ tests/RequestTest.php
@@ -0,0 +1,74 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hong
+ * Date: 16-10-29
+ * Time: 下午3:17
+ */
+
+namespace Tests\PhMessage;
+
+
+use PhMessage\FnStream;
+use PhMessage\Request;
+use PhMessage\Uri;
+use PHPUnit\Framework\TestCase;
+
+class RequestTest extends TestCase
+{
+    public function testRequestUriMeBeString()
+    {
+        $r = new Request('GET', '/');
+        $this->assertEquals('/', $r->getUri());
+    }
+
+    public function testRequestUriMayBeUri()
+    {
+        $uri = new Uri('/');
+        $r = new Request('GET', $uri);
+
+        $this->assertEquals('/', $r->getUri());
+    }
+
+    public function testValidateRequestUri()
+    {
+        $r = new Request('GET', '/', [], 'baz');
+
+        $this->assertInstanceOf('Psr\Http\Message\StreamInterface', $r->getBody());
+
+        $this->assertEquals('baz', (string) $r->getBody());
+    }
+
+    public function testNullBody()
+    {
+        $r = new Request('GET', '/', [], null);
+        $this->assertInstanceOf('Psr\Http\Message\StreamInterface', $r->getBody());
+        $this->assertSame('', (string) $r->getBody());
+    }
+
+    public function testFalseyBody()
+    {
+        $r = new Request('GET', '/', [], '0');
+        $this->assertInstanceOf('Psr\Http\Message\StreamInterface', $r->getBody());
+        $this->assertSame('0', (string) $r->getBody());
+    }
+
+
+    public function testConstructorDoseNotReadStreamBody()
+    {
+        $streamIsRead = false;
+
+        $body = FnStream::decorate(\PhMessage\stream_for(''), [
+            '__toString' => function () use (&$streamIsRead) {
+                $streamIsRead = true;
+                return '';
+            }
+        ]);
+
+        $r = new Request('GET', '/', [], $body);
+
+        $this->assertFalse($streamIsRead);
+        $this->assertSame($body, $r->getBody());
+
+    }
+}
---
+++ tests/StreamTest.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hong
+ * Date: 16-10-29
+ * Time: 下午4:24
+ */
+
+namespace Tests\PhMessage;
+
+
+use PhMessage\Stream;
+use PHPUnit\Framework\TestCase;
+
+class StreamTest extends TestCase
+{
+
+    /**
+     * @expectedException \InvalidArgumentException
+     */
+    public function testConstructorThrowsExceptionOnInvalidArgument()
+    {
+        new Stream(true);
+    }
+
+    public function testConstructorInitializesProperties()
+    {
+        $handle = fopen('php://temp', 'r+');
+        fwrite($handle, 'data');
+
+        $stream = new Stream($handle);
+        $this->assertTrue($stream->isReadable());
+        $this->assertTrue($stream->isWritable());
+        $this->assertTrue($stream->isSeekable());
+        $this->assertEquals('php://temp', $stream->getMetadata('uri'));
+        $this->assertInternalType('array', $stream->getMetadata());
+        $this->assertEquals(4, $stream->getSize());
+        $this->assertFalse($stream->eof());
+
+        $stream->close();
+    }
+
+    public function testStreamClosesHandleOnDestruct()
+    {
+        $handle = fopen('php://temp', 'r');
+        $stream = new Stream($handle);
+        unset($stream);
+        $this->assertFalse(is_resource($handle));
+    }
+}
